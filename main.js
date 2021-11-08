9f2351c5a7fbf3ee57ecb3e8bdae9fdf00b06ff4
Ruchira Sachinthana <ruchira.sachinthana.1@gmail.com>
2021-11-08 05:44:34 +0530

    updated

--- main.js
+++ main.js
@@ -1,15 +1,25 @@
-function test(id){
+// function test(id){
 
-    alert(id);
+//     alert(id);
 
-}
+// }
 
 
-test01 = function(id){
+// test01 = function(id){
 
-    alert(id);
-}
+//     alert(id);
+// }
 
 // arrow function
 
+test2 =(id) =>{
+
+    alert(id);
+}
+
+
 test3 = (id) => alert(id);
+
+calSum = (a,b) =>alert("Sum is :-" +(a+b));
+
+test4 = () =>alert(document.getElementById('name').value);
