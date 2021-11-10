a4c17cfe09bef9775a041c0aaaba94407825bb95
Ruchira Sachinthana <ruchira.sachinthana.1@gmail.com>
2021-11-10 19:05:31 +0530

    updated

--- main.js
+++ main.js
@@ -23,3 +23,16 @@ test3 = (id) => alert(id);
 calSum = (a,b) =>alert("Sum is :-" +(a+b));
 
 test4 = () =>alert(document.getElementById('name').value);
+
+
+// define array
+
+var colors = ["red","green","blue"];
+// alert(colors[1]);
+document.getElementById("display").innerHTML = colors;
+
+var cars = [];
+cars[0]= "Toyota";
+cars[1]= "Nissan";
+cars[2]= "Mazda";
+document.getElementById("car").innerHTML = cars;
