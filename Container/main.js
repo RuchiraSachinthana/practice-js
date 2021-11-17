39150090037f6fabd137a73bade64f3584058f7b
Ruchira Sachinthana <ruchira.sachinthana.1@gmail.com>
2021-11-18 00:26:02 +0530

    update

--- Container/main.js
+++ Container/main.js
@@ -1,4 +1,7 @@
-function ShowTab(TabNumber){
-console.log(TabNumber);
-
+function ShowTab(tabNumber){
+console.log(tabNumber);
+document.getElementsByClassName("show")[0].classList.add("hide");
+document.getElementsByClassName("show")[0].classList.remove("show");
+document.getElementById("tabcontent-" + tabNumber).classList.add("show");
+document.getElementById("tabcontent-" + tabNumber).classList.remove("hide");
 }
