7087dbede8ab5fd45454ff61c2c23b64289cdb85
Ruchira Sachinthana <ruchira.sachinthana.1@gmail.com>
2021-11-17 23:52:22 +0530

    updated

--- clock/main.js
+++ clock/main.js
@@ -8,9 +8,9 @@ function updateClock(){
     var my_minutes = my_date.getMinutes();
     var my_seconds = my_date.getSeconds();
 
-if (my_hours <10 ) { my_hours = "0" + my_hours ; }
-if (my_minutes <10 ) { my_minutes = "0" + my_minutes ; }
-if (my_seconds <10 ) { my_seconds = "0" + my_seconds ; }
+if (my_hours < 10 ) { my_hours = "0" + my_hours ; }
+if (my_minutes < 10 ) { my_minutes = "0" + my_minutes ; }
+if (my_seconds < 10 ) { my_seconds = "0" + my_seconds ; }
  
 var My_clock = my_hours + ":" + my_minutes + ":" + my_seconds ;
 document.getElementById("clock").innerText = My_clock ;
