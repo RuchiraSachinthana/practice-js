d046632de1fbd4a6bad081500aba9938c17ab553
Ruchira Sachinthana <ruchira.sachinthana.1@gmail.com>
2021-11-20 22:37:49 +0530

    updated

--- Drop-Down/main.js
+++ Drop-Down/main.js
@@ -1,7 +1,7 @@
 var nav = document.getElementById("nav");
 
 nav.addEventListener("change",function(){
-    window.open(nav.value,"_self");
+    window.open(nav.value, "mywinndow", "width=300, height=200");
 console.log(nav.value);
 
 });
