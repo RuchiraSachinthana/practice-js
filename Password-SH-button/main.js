b3c20189eccc67fc349d36f576798b99b87536da
Ruchira Sachinthana <ruchira.sachinthana.1@gmail.com>
2021-11-21 23:05:54 +0530

    updated

--- Password-SH-button/main.js
+++ Password-SH-button/main.js
@@ -1,10 +1,22 @@
 var eye = document.getElementById("eye");
 var password = document.getElementById("password");
+var showPassword = false;
 
 eye.addEventListener("click", function(){
+if(showPassword == false){
+
 password.setAttribute("type", "text");
 eye.classList.add("far fa-eye-slash");
 eye.classList.remove("far fa-eye");
+showPassword = true;
+}
 
+else{
+
+    password.setAttribute("type", "text");
+    eye.classList.remove("far fa-eye-slash");
+    eye.classList.add("far fa-eye");
+    showPassword = false;
+}
 
 });
