e488ded3b6d3d24d5fefa0a3c981e566782f6f9a
Ruchira Sachinthana <ruchira.sachinthana.1@gmail.com>
2021-11-22 17:16:20 +0530

    updated

--- selections/main.js
+++ selections/main.js
@@ -1,1 +1,18 @@
 console.log("Hi")
+
+var CheckBoxes = document.getElementsByClassName("chkbx");
+
+var SelectAll = document.getElementById("selall");
+var SelectNone = document.getElementById("selnone");
+
+SelectAll.addEventListener("click", function(e){
+    e.preventDefault();
+for(var i = 0 ; i < CheckBoxes.length; i++)
+CheckBoxes[i].checked = true ;
+}) ;
+
+SelectNone.addEventListener("click", function(d){
+    d.preventDefault();
+for(var i = 0 ; i < CheckBoxes.length; i++)
+CheckBoxes[i].checked = false ;
+})
