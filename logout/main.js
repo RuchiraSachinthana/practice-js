111a232daf11365a0764ba00c2f1a44ee5a33ca8
Ruchira Sachinthana <ruchira.sachinthana.1@gmail.com>
2021-11-22 17:44:58 +0530

    updated

--- logout/main.js
+++ logout/main.js
@@ -1,5 +1,27 @@
-setTimeout(logoutUser,5000);
+// setTimeout(logoutUser,5000);
 
-function logoutUser(){
-    window.location = "logout.html" ;
+// function logoutUser(){
+//     window.location = "logout.html" ;
+// }
+
+var timer = document.getElementById("timer");
+var duration = 5; //duration set to 5 seconds
+
+setInterval(updateTimer, 1000);
+
+function updateTimer(){
+    duration--;
+    if(duration < 1){
+        window.location = "logout.html" ;
+    } else{
+        timer.innerText = duration;
+
+    }
+
+}
+
+window.addEventListener("mousemove", resetTimer);
+
+function resetTimer(){
+    duration = 5;
 }
