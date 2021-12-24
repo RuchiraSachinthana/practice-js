d01e71046334afa3c60687eb0de2856d9cec1525
Ruchira Sachinthana <ruchira.sachinthana.1@gmail.com>
2021-12-24 22:55:17 +0530

    updated

--- callback_function/main.js
+++ callback_function/main.js
@@ -1,14 +1,18 @@
-function function_one(callback) { // callback kyna parameter ekt kamathi namak dena plwn
+function function_one(cbfunction) { // cbfunction kyna parameter ekt kamathi namak dena plwn
     console.log('This is Function one');
-    callback();
+    const randNum = Math.random();
+    cbfunction(randNum);
+
 }
 
 function function_two() {
     console.log('This is Function two');
 }
 
-function function_three() {
+function function_three(randNum) {
     console.log('This is Function three');
+    console.log(randNum)
 }
 
-function_one(function_two);
+function_one(function_three);
+
