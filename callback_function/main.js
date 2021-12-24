4ca81d65c5d7b66a731ce68b6df360e753292dbf
Ruchira Sachinthana <ruchira.sachinthana.1@gmail.com>
2021-12-24 22:39:21 +0530

    updated

--- callback_function/main.js
+++ callback_function/main.js
@@ -1,3 +1,11 @@
 function function_one() {
-    console.log('This is Function one')
+    console.log('This is Function one');
+}
+
+function function_two() {
+    console.log('This is Function two');
+}
+
+function function_three() {
+    console.log('This is Function three');
 }
