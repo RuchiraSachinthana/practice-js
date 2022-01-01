60a53b30a638dc5369ab7ffd4bcd40f730dbeb8b
Ruchira Sachinthana <ruchira.sachinthana.1@gmail.com>
2022-01-01 20:53:32 +0530

    updated

--- spread Operator/main.js
+++ spread Operator/main.js
@@ -1,8 +1,18 @@
 // joining arrays
 
-const workingDays = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri'];
+// const workingDays = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri'];
 
-const restDays = ['Sat', 'Sun'];
+// const restDays = ['Sat', 'Sun'];
 
-const daysOfweek = [...workingDays,...restDays];
-console.log(daysOfweek);
+// const daysOfweek = [...workingDays,...restDays];
+// console.log(daysOfweek);
+
+// using function
+
+function add(num1,num2){
+    console.log('Sum:',num1 + num2);
+}
+
+const numbers = [4,5];
+
+add(numbers[0],numbers[1])
