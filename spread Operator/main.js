de47c73d4e00834c18eca2216d614a1a58d65187
Ruchira Sachinthana <ruchira.sachinthana.1@gmail.com>
2022-01-01 22:20:47 +0530

    updated

--- spread Operator/main.js
+++ spread Operator/main.js
@@ -9,10 +9,28 @@
 
 // using function
 
-function add(num1,num2){
-    console.log('Sum:',num1 + num2);
+// function add(num1,num2){
+//     console.log('Sum:',num1 + num2);
+// }
+
+// const numbers = [4,5];
+
+// //add(numbers[0],numbers[1])
+
+// add(...numbers);
+
+
+const car = {
+    make:"Honda",
+    model: "Fit",
+    year: 2012
 }
 
-const numbers = [4,5];
+const modification = {
+    headlight: "LED",
+    seats: "Leather"
+}
 
-add(numbers[0],numbers[1])
+const modifiedCar = { ...car, ...modification}
+
+console.log(modifiedCar);
