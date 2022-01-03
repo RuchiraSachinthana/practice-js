1348f5925c4f300986267082f1ad51780deb779a
Ruchira Sachinthana <ruchira.sachinthana.1@gmail.com>
2022-01-03 22:24:38 +0530

    updated

---
+++ Destructure/main.js
@@ -0,0 +1,7 @@
+const daysOfweek = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'];
+
+let[first, second, , , fifth] = daysOfweek;
+
+console.log(first);
+console.log(second);
+console.log(fifth);
