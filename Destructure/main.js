155ab2a71e31bac4fd7fd270b46ace01db63439c
Ruchira Sachinthana <ruchira.sachinthana.1@gmail.com>
2022-01-03 22:31:04 +0530

    updated

--- Destructure/main.js
+++ Destructure/main.js
@@ -1,7 +1,8 @@
 const daysOfweek = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'];
 
-let[first, second, , , fifth] = daysOfweek;
+let[first, second, , , fifth, ...others] = daysOfweek;
 
 console.log(first);
 console.log(second);
 console.log(fifth);
+console.log(others);
---
+++ Destructure/script.js
@@ -0,0 +1,17 @@
+const car = {
+    make: "Honda",
+    model: "Fit",
+    year: 2020,
+    engine: {
+        capaacity: 3.6,
+        type: "v6"
+    }
+}
+
+const {
+    model,
+    engine
+} = car;
+console.log(model);
+console.log(engine);
+
