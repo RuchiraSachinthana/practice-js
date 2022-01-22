51bd2513fdddb271c4b26aa29648f3f5903ddf70
Ruchira Sachinthana <ruchira.sachinthana.1@gmail.com>
2022-01-22 21:57:51 +0530

    updated

--- forEach-Method/main.js
+++ forEach-Method/main.js
@@ -7,3 +7,12 @@ function callbackFunction(value, index) {
  // function body
 }
 */
+
+function addTax(price, index) {
+    console.log(index, (price * 1.15).toFixed(2));
+}
+
+const prices = [1234, 234, 3451, 432];
+
+prices.forEach(addTax);
+
