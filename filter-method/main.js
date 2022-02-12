f101924e3287829c6ffc6933b8f03e6662b37cc4
Ruchira Sachinthana <ruchira.sachinthana.1@gmail.com>
2022-02-12 21:52:17 +0530

    created filter method file

---
+++ filter-method/main.js
@@ -0,0 +1,21 @@
+/*
+const prices = [150, 100, 60, 80, 95, 130, 65, 400, 210 ];
+
+
+
+function checkPrice(price) {
+ if (price < 100) {
+        return true;
+    }
+}
+
+
+
+const lowPrices = prices.filter(price => price < 100);
+const highPrices = prices.filter(price => price >= 100);
+
+console.log("Prices: " , prices);
+console.log("Low Prices: " , lowPrices);
+console.log("High Prices: " , highPrices);
+
+ */
