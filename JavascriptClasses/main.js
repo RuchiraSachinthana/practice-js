2689d80a742b102df507b6cfef1775d5e70fd106
Ruchira Sachinthana <ruchira.sachinthana.1@gmail.com>
2022-04-11 23:42:42 +0530

    updated

--- JavascriptClasses/main.js
+++ JavascriptClasses/main.js
@@ -10,10 +10,19 @@ class Person {
 }
 
 class Employee extends Person{
+
+    #salary;
     constructor(fname, lname, possition, salary) {
         super(fname, lname);
         this.possition = possition;
-        this.salary = salary;
+        this.#salary = salary;
+    }
+    introduce() {
+        console.log(`Hi, My name is ${this.fname} ${this.lname} and I am a ${this.possition}`);
+    }
+
+    showSalary() {
+        console.log(`My salary is ${this.#salary}`);
     }
 }
 
@@ -21,3 +30,9 @@ const Ruchira = new Person('Ruchira', 'Sachinthana');
 const Kamal = new Employee('Kamal', 'Perera', 'Software Developer', '200000');
 
 Ruchira.introduce();
+
+Kamal.introduce();
+Kamal.salary = 225000;
+
+console.log(Kamal.salary);
+console.log.(showS)
